585a2c0fa390f2ff0898f8e5e11e1307c96360c1
Daber98 <dberduoh@miumg.edu.gt>
2024-05-31 22:46:00 -0600

    Añadiendo cambios

--- server/index.js
+++ server/index.js
@@ -1,46 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connection = require("./db");
+const MongoDBConnection = require("./db");
 const crudRoutes = require("./routes/crudRoutes");
 
 const app = express();
 
-// database connection
-connection();
+// Conexión a la base de datos
+MongoDBConnection();
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// CORS configuration
-const allowedOrigins = [
-	'http://shoppr-web-alb-1257490238.us-east-1.elb.amazonaws.com',
-	'http://shoppr-web-alb-1257490238.us-east-1.elb.amazonaws.com'
-  ];
-  
-  app.use(cors({
-	origin: function (origin, callback) {
-	  // Allow requests with no origin
-	  // (like mobile apps or curl requests)
-	  if (!origin) return callback(null, true);
-	  if (allowedOrigins.indexOf(origin) === -1) {
-		const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-		return callback(new Error(msg), false);
-	  }
-	  return callback(null, true);
-	},
-	credentials: true
-  }));
-
+app.use(cors());
 app.use((req, res, next) => {
 	res.locals.path = req.path;
 	next();
 });
 
-// routes
+// Rutas
 app.use("/api/cruds", crudRoutes);
 
-// listening on port
+// Puerto
 const port = 4000
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Server iniciado en el puerto: ${port}...`));
