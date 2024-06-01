11e4cc8be28980b091fc78d24a6a89f0fe675206
Daber98 <dberduoh@miumg.edu.gt>
2024-05-31 22:54:57 -0600

    test

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ MongoDBConnection();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
 app.use((req, res, next) => {
 	res.locals.path = req.path;
 	next();
