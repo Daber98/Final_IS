10d172d21d7707431d6d8e6f59fac035c97d0751
Daber98 <dberduoh@miumg.edu.gt>
2024-05-31 22:57:28 -0600

    test

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ MongoDBConnection();
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://shoppr-web-alb-108717195.us-east-1.elb.amazonaws.com'
+}));
 
 app.use((req, res, next) => {
 	res.locals.path = req.path;
