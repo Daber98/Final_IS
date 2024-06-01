8523e5b5659bf10b89d093d73b08fec2c602f32b
Daber98 <dberduoh@miumg.edu.gt>
2024-05-31 23:39:10 -0600

    test

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ connection();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://shoppr-web-alb-974193603.us-east-1.elb.amazonaws.com'
+}));
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
